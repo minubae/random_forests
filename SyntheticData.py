e6f8242ac8c1b3f60a9d5b29b40335809c1213a7
minwoobae <minubae.nyc@gmail.com>
2017-12-04 17:26:44 -0500

    modified: SyntheticData.py

--- SyntheticData.py
+++ SyntheticData.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import bernoulli,poisson,norm,expon
 from random import *
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+import graphviz
 
 # nobs = 100
 # theta = 0.3
@@ -13,14 +16,14 @@ from random import *
 # print(expon.rvs(5,size = N))
 
 # Number of Variables (Features)
-p = 3
+p = 4
 # Number of Observations
-n = 10
+n = 20
 # probability of Each Bernoulli trial
 prob =.5
 # Synthetic Data Matrix X
 X = []
-
+testX  = []
 for i in range(p):
     #x_i is a random variable for each feature
     x_i = bernoulli.rvs(prob,size = n)
@@ -28,8 +31,16 @@ for i in range(p):
 
 X = np.transpose(np.array(X))
 
-print('Data Matrix X:')
-print(X)
+# print('Data Matrix X:')
+# print(X)
+
+for j in range(p):
+    #x_i is a random variable for each feature
+    x_j = bernoulli.rvs(prob,size = n)
+    testX.append(x_j)
+
+testX = np.transpose(np.array(testX))
+
 
 # logical_or function satisfying multiple conditions
 def logical_or(inputX):
@@ -62,17 +73,51 @@ def logical_and(inputX):
 
 #Training Data Z
 Z = []
+Y =[]
 for i, x in enumerate(X):
-    print('x: ', x)
+    # print('x: ', x)
     y = logical_or(x)
-    y2 = logical_and(x)
-    print('y (logical_or): ', y)
-    print('y (logical_and): ', y2)
-    x = np.append(x,y)
-    print('z: ', x)
-    Z.append(x)
-    print('')
+    # y = logical_and(x)
+    # print('y (logical_or): ', y)
+    # print('y (logical_and): ', y2)
+    Y.append(y)
+    # x = np.append(x,y)
+    # print('z: ', x)
+    # Z.append(x)
+    # print('')
+testY = []
 
-Z = np.array(Z)
-print('Training Data:')
-print(Z)
+for j, x in enumerate(testX):
+    # print('x: ', x)
+    y = logical_or(x)
+    # y = logical_and(x)
+    # print('y (logical_or): ', y)
+    # print('y (logical_and): ', y2)
+    testY.append(y)
+
+
+# Z = np.array(Z)
+Y = np.array(Y)
+print('Original Data:')
+print(X)
+print('Train_Predict:')
+print(Y)
+
+
+print('Test Data:')
+print(testX)
+print('Test_Predict:')
+print(testY)
+
+clf = tree.DecisionTreeRegressor()
+clf = clf.fit(X,Y)
+
+tree_predict = []
+for i in range(10):
+    predict = clf.predict(testX)
+    tree_predict.append(np.array(predict))
+# predict = clf.predict(testX)
+
+tree_predict = np.array(tree_predict)
+print('DecisionTreePredict: ')
+print(tree_predict)
